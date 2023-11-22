876e74c1219e4434d4283e1639dd2344318f3e93
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2023-11-22 20:30:11 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -15,5 +15,5 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
     # if getenv("HBNB_TYPE_STORAGE") == "db":
-    state = relationship('State', backref='cities')
+    # state = relationship('State', backref='cities')
     places = relationship("Place", backref="cities", cascade="delete")
