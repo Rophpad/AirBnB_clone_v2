a28132eae4079b8fde276bf88fa6ab301a256c30
Rophpad <rophenp@gmail.com>
2023-12-07 16:25:27 +0100

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,8 +7,6 @@ from fabric.api import put, run, env
 from os.path import exists
 
 env.hosts = ['52.72.14.202', '18.204.9.164']
-env.user = 'ubuntu'
-env.identity = '~/.ssh/school'
 env.password = None
 
 
