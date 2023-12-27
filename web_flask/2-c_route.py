97ab389bcf6acd89db5cce36a0514e8e773ba981
Rophpad <rophenp@gmail.com>
2023-12-27 21:16:25 +0100

    Add route /c/<text> to previous script

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Start a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Return Hello HBNB! from 0.0.0.0:5000 """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns HBNH from 0.0.0.0:5000/hbnb """
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c_route(text):
+    """ Return C followed by the value of text """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
