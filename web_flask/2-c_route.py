e904d818900c2749a1d1ec197b01d48c54a0bafc
Rophpad <rophenp@gmail.com>
2023-12-27 21:18:29 +0100

    updated

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,7 +15,7 @@ def hbnb():
     """ Returns HBNH from 0.0.0.0:5000/hbnb """
     return "HBNB"
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     """ Return C followed by the value of text """
     text = text.replace('_', ' ')
