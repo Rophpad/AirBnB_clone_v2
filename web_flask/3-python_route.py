bb14c87236b09b405a4a0f42cb4fe1ad007fba6f
Rophpad <rophenp@gmail.com>
2023-12-27 21:31:34 +0100

    /python/<text> route added

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Start a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Return Hello HBNB! from 0.0.0.0:5000 """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns HBNH from 0.0.0.0:5000/hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ Return C followed by the value of text """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_route(text="is cool"):
+    """ Returns Python followed by the vale of the text """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
